9181b52a57bf67c6c1794221faf31f3d61778ae7
bennisan86 <info@bennisan.be>
2023-10-05 12:35:46 +0200

    Syncen voor de lunch!

--- frontend/components/mint/ShoppingCart.tsx
+++ frontend/components/mint/ShoppingCart.tsx
@@ -47,14 +47,14 @@ export const ShoppingCart = ({ onClose }) => {
 
 
     return (
-        <>
-            <button onClick={() => { buyPopup() }} disabled={isEmpty} className="flex-1 md:flex-none md:w-full px-0 middlerounded bg-white border-[1px] border-sgorange2 text-sgorange2">show cart ({totalUniqueItems})</button>
+        <div className="flex-1 md:flex-none md:w-full">
+            <button onClick={() => { buyPopup() }} disabled={isEmpty} className="w-full px-0 middlerounded bg-white border-[1px] border-sgorange2 text-sgorange2">show cart ({totalUniqueItems})</button>
             {isEmpty ?
                 null :
                 <p className="w-full text-[14px] text-sgbrown font-bold mt-[10px] mb-[10px] text-center">Total: {utils.formatEther(cartTotal)} ETH</p>
             }
             {showBuyConfirmation && (<MintConfirmation onClose={() => { SetShowBuyConfirmation(false); onClose() }} />)}
 
-        </>
+        </div>
     );
 };
