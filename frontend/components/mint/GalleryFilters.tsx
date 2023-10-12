0d6be337411141206ae706f8c78418fd45e0f802
sponnet <stefaan@ponnet.com>
2023-10-12 14:58:38 +0200

    fix nasty SSR bug

--- frontend/components/mint/GalleryFilters.tsx
+++ frontend/components/mint/GalleryFilters.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState, Fragment } from "react";
 import { Disclosure, Dialog, Transition } from '@headlessui/react'
 import { MinusIcon, PlusIcon } from '@heroicons/react/20/solid'
 import { useCollectionFilter } from "hooks/useCollectionFilter";
-import { traits } from "../../data/traits.json";
+import traitsData from "../../data/traits.json";
 
 const CheckboxGroup = ({ data }) => {
 
@@ -93,7 +93,7 @@ const CheckboxGroup = ({ data }) => {
 
 const FilterRadioGroup = () => {
   return (
-    <CheckboxGroup data={traits} />
+    <CheckboxGroup data={traitsData.traits} />
   )
 }
 
@@ -117,13 +117,13 @@ export const GalleryFilters = () => {
     setIsInfoOpen(false)
   }
   const openInfoModal = () => {
-    setIsInfoOpen(true) 
+    setIsInfoOpen(true)
   }
 
 
   return (
     <>
-    <Transition appear show={isFilterOpen} as={Fragment}>
+      <Transition appear show={isFilterOpen} as={Fragment}>
         <Dialog as="div" className="relative z-[9999]" onClose={closeFilterModal}>
           <Transition.Child
             as={Fragment}
@@ -156,7 +156,7 @@ export const GalleryFilters = () => {
                   </Dialog.Title>
                   <FilterRadioGroup />
                   <div className="flex flex-row justify-end items-center">
-                      <p className="text-[16px] leading-[20px] underline mt-3 text-sgorange" onClick={() => closeFilterModal()}>close</p>
+                    <p className="text-[16px] leading-[20px] underline mt-3 text-sgorange" onClick={() => closeFilterModal()}>close</p>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
@@ -199,7 +199,7 @@ export const GalleryFilters = () => {
                   </Dialog.Title>
                   <p>next price stuff here</p>
                   <div className="flex flex-row justify-end items-center">
-                      <p className="text-[16px] leading-[20px] underline mt-3 text-sgorange" onClick={() => closeInfoModal()}>close</p>
+                    <p className="text-[16px] leading-[20px] underline mt-3 text-sgorange" onClick={() => closeInfoModal()}>close</p>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
@@ -209,32 +209,32 @@ export const GalleryFilters = () => {
       </Transition>
 
 
-    <div className="md:w-full flex-row md:flex-col justify-start items-center pt-[20px] pb-[0px]">
-      {/* MOBILE HERE */}
-      <div className="flex md:hidden  flex-row justify-center ml-[10px]">
-        <div onClick={() => openFilterModal()} className="flex flex-row justify-center w-[30px] h-[20px] mx-[5px] hover:cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" stroke="#59342B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-filter"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon></svg>
+      <div className="md:w-full flex-row md:flex-col justify-start items-center pt-[20px] pb-[0px]">
+        {/* MOBILE HERE */}
+        <div className="flex md:hidden  flex-row justify-center ml-[10px]">
+          <div onClick={() => openFilterModal()} className="flex flex-row justify-center w-[30px] h-[20px] mx-[5px] hover:cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" stroke="#59342B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-filter"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon></svg>
+          </div>
+          <div onClick={() => openInfoModal()} className="flex flex-row justify-center w-[30px] h-[20px] mx-[5px] hover:cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" stroke="#59342B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
+          </div>
         </div>
-        <div onClick={() => openInfoModal()}className="flex flex-row justify-center w-[30px] h-[20px] mx-[5px] hover:cursor-pointer">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" fill="none" stroke="#59342B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
-        </div>
-      </div>
 
-      {/* DESKTOP HERE */}
-      <div className="hidden md:block">
-      <div className="md:flex flex-row items-center justify-center cursor-pointer">
-        <div className={isOpen ? "transition-all ease-in-out w-[12px] h-[12px] rotate-90 mr-2" : " transition-all ease-in-outw-[9px] h-[12px] rotate-0  mr-2"}>
-          <svg width="8" height="10" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 1L7 4.73333L1 9" stroke="#59342B" />
-          </svg>
+        {/* DESKTOP HERE */}
+        <div className="hidden md:block">
+          <div className="md:flex flex-row items-center justify-center cursor-pointer">
+            <div className={isOpen ? "transition-all ease-in-out w-[12px] h-[12px] rotate-90 mr-2" : " transition-all ease-in-outw-[9px] h-[12px] rotate-0  mr-2"}>
+              <svg width="8" height="10" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 1L7 4.73333L1 9" stroke="#59342B" />
+              </svg>
+            </div>
+            <p className="text-[16px] leading-[20px] underline" onClick={() => toggleIsOpen(!isOpen)}>filter & sort</p>
+          </div>
+          <div className={isOpen ? "block mt-[30px] lg:mt-[10px] lg:mb-[0px]" : "hidden"}>
+            <FilterRadioGroup />
+          </div>
         </div>
-        <p className="text-[16px] leading-[20px] underline" onClick={() => toggleIsOpen(!isOpen)}>filter & sort</p>
       </div>
-      <div className={isOpen ? "block mt-[30px] lg:mt-[10px] lg:mb-[0px]" : "hidden"}>
-        <FilterRadioGroup />
-      </div>
-      </div>
-    </div>
     </>
   )
 }
